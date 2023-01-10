749bfb5866d04eddd6daedcd42dba82c13f9afa1
jobmania <xzxz7003@naver.com>
2023-01-10 16:45:03 +0900

    2023-01-10

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,13 +14,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+
+    //"call AppConfig.memberService"
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //"call AppConfig.orderService"
+    //call AppConfig.memberRepository
+
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
@@ -28,6 +40,7 @@ public class AppConfig {
     // 변경시 return 바꿔주만 됨 ㅆ //appcofing 역할과 구현에 한눈에 보임
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core", // 멤버 패키지만 컴포넌트 스캔의 대상이 됨
+        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다 여기서는 hello.core;
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
